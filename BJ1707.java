977e2168f9fa0f22cfcab551c4d65c6d5cf7b0b2
seazero <withbb96@gmail.com>
2021-10-08 22:48:13 +0900

    예외케이스 찾기
    ㅂ
    q

--- BJ1707.java
+++ BJ1707.java
@@ -1,3 +1,4 @@
+
 /*이분 그래프 
 그래프의 정점의 집합을 둘로 분할하여, 
 각 집합에 속한 정점끼리는 서로 인접하지 않도록 분할할 수 있을 때, 
@@ -5,53 +6,80 @@
 
 그래프가 입력으로 주어졌을 때, 이 그래프가 이분 그래프인지 아닌지 판별하는 프로그램을 작성하시오.
 */
+import java.io.*;
 import java.util.*;
 
 public class BJ1707 {
-    public static boolean dfs(ArrayList<Integer>[] a, int[] color, int x, int c) {
-        color[x] = c;
-        for (int y : a[x]) {
-            if (color[y] == 0) {
-                if (dfs(a, color, y, 3-c) == false) {
-                    return false;
-                }
-            } else if (color[y] == color[x]) {
-                return false;
+    static int K;
+    static int V;
+    static int E;
+    static int u;
+    static int v;
+    static ArrayList<ArrayList<Integer>> Bipartite;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        // 사이클이 생기지 않는지 확인하면 되는건가??
+        // 4 4
+        // 2 1
+        // 3 2
+        // 4 3
+        // 4 1
+        K = Integer.parseInt(br.readLine()); // 테케 수
+
+        while (K-- > 0) {
+            Bipartite = new ArrayList<ArrayList<Integer>>();
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            V = Integer.parseInt(st.nextToken());
+            E = Integer.parseInt(st.nextToken());
+
+            visited = new boolean[V + 1];
+
+            for (int i = 0; i < V + 1; i++) {
+                Bipartite.add(new ArrayList<Integer>());
             }
-        }
-        return true;
-    }
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        while (t-- > 0) {
-            int n = sc.nextInt();
-            int m = sc.nextInt();
-            ArrayList<Integer>[] a = (ArrayList<Integer>[]) new ArrayList[n+1];
-            for (int i=1; i<=n; i++) {
-                a[i] = new ArrayList<Integer>();
+
+            for (int i = 0; i < E; i++) {
+                st = new StringTokenizer(br.readLine());
+                u = Integer.parseInt(st.nextToken());
+                v = Integer.parseInt(st.nextToken());
+                Bipartite.get(u).add(v);
+                Bipartite.get(v).add(u);
             }
-            for (int i=0; i<m; i++) {
-                int u = sc.nextInt();
-                int v = sc.nextInt();
-                a[u].add(v);
-                a[v].add(u);
-            }
-            int[] color = new int[n+1];
-            boolean ok = true;
-            for (int i=1; i<=n; i++) {
-                if (color[i] == 0) {
-                    if (dfs(a, color, i, 1) == false) {
-                        ok = false;
-                    }
-                }
-            }
-            if (ok) {
-                System.out.println("YES");
+
+            // DFS(1, -1);
+
+            if (DFS(1, -1)) {
+                bw.write(String.valueOf("NO\n")); // 사이클이 있어서 이분 그래프가 아니다
             } else {
-                System.out.println("NO");
+                bw.write(String.valueOf("YES\n")); // 이분 그래프다
+            }
+
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+
+    public static boolean DFS(int now, int parent) {
+        visited[now] = true;
+        for (int next : Bipartite.get(now)) {
+            if (!visited[next]) {
+                if (DFS(next, now)) {
+
+                    return true;
+                }
+            } else if (next != parent) {
+                return true;
             }
         }
+        return false;
 
     }
 }
