d8c63180fd3087e39278c51afc36e9b7ffa9657c
seazero <withbb96@gmail.com>
2021-10-10 15:04:47 +0900

    시간초과

--- BJ2667.java
+++ BJ2667.java
@@ -1,3 +1,4 @@
+
 /* 단지번호붙이기
 <그림 1>과 같이 정사각형 모양의 지도가 있다. 
 1은 집이 있는 곳을, 0은 집이 없는 곳을 나타낸다. 
@@ -6,144 +7,68 @@
 대각선상에 집이 있는 경우는 연결된 것이 아니다. <그림 2>는 <그림 1>을 단지별로 번호를 붙인 것이다. 
 지도를 입력하여 단지수를 출력하고, 각 단지에 속하는 집의 수를 오름차순으로 정렬하여 출력하는 프로그램을 작성하시오.
 */
+import java.io.*;
 import java.util.*;
-//DFS
+
 public class BJ2667 {
-    private static int dx[] = {0,0,1,-1};
-    private static int dy[] = {1,-1,0,0};
-    private static int[] Apt = new int[25*25];
-    private static int n;
-    private static int apartNum = 0; //아파트 단지 번호의 수
-    private static boolean[][] visited = new boolean[25][25]; //방문여부
-    private static int[][] map = new int[25][25]; //지도
+    public static int dx[] = { 0, 0, 1, -1 };
+    public static int dy[] = { 1, -1, 0, 0 };
+    public static int[] danji;
+    public static int N;
+    public static int apartNum = 0; // 아파트 단지 번호의 수
+    public static boolean[][] visited; // 방문여부
+    public static ArrayList<String> Apt;
 
-    private static void dfs(int x, int y) {
-        visited[x][y] = true; //방문하면 T
-        Apt[apartNum]++; //단지 카운트
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        for(int i=0; i<4; i++){
+        N = Integer.parseInt(br.readLine());
+        Apt = new ArrayList<String>();
+        visited = new boolean[N][N];
+        danji = new int[N + 1]; // 단지에 아파트 몇 채 있는지 카운트 담기
+
+        for (int i = 0; i < N; i++) { // 아파트 위치 입력받기
+            Apt.add(br.readLine());
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (Apt.get(i).charAt(j) == '1' && !visited[i][j]) {
+                    apartNum++;
+                    DFS(i, j);
+                }
+            }
+        }
+
+        Arrays.sort(danji);
+        bw.write(String.valueOf(apartNum) + "\n");
+
+        for (int i = 0; i < danji.length; i++) {
+            if (danji[i] == 0) {
+            } else {
+                bw.write(danji[i] + "\n");
+            }
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    public static void DFS(int x, int y) {
+        visited[x][y] = true; // 방문하면 T
+        danji[apartNum]++; // 단지속 아파트 카운트
+
+        for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
 
-            if(nx >=0 && ny >=0 && nx < n && ny < n){ //지도 범위 안에 들었는지 확인
-                if(map[nx][ny] == 1 && !visited[nx][ny]){ //아파트가 있고, 방문안한 곳
-                    dfs(nx,ny);
-                }
-            }
-        }
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-
-        map = new int[n][n];
-        visited = new boolean[n][n];
-
-        //전체 사각형 입력 받기
-        for(int i=0; i<n; i++){
-            String input = sc.next();
-            for(int j=0; j<n; j++){
-                map[i][j] = input.charAt(j)-'0';
-            }
-        }
-
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(map[i][j] == 1 && !visited[i][j]){
-                    apartNum++;
-                    dfs(i,j);
-                }
-            }
-        }
-
-        Arrays.sort(Apt);
-        System.out.println(apartNum);
-
-        for(int i=0; i<Apt.length; i++){
-            if(Apt[i] == 0){
-            }else{               
-                System.out.println(Apt[i]);
-            }
-        }
-    }
-}
-
-
-/*
-//DFS
-public class BJ2667 {
-    private static int dx[] = {0,0,1,-1};
-    private static int dy[] = {1,-1,0,0};
-    private static int[] aparts = new int[25*25];
-    private static int n;
-    private static int apartNum = 0; //아파트 단지 번호의 수
-    private static boolean[][] visited = new boolean[25][25]; //방문여부
-    private static int[][] map = new int[25][25]; //지도
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-
-        map = new int[n][n];
-        visited = new boolean[n][n];
-
-        //전체 사각형 입력 받기
-        for(int i=0; i<n; i++){
-            String input = sc.next();
-            for(int j=0; j<n; j++){
-                map[i][j] = input.charAt(j)-'0';
-             }
-        }
-
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(map[i][j] == 1 && !visited[i][j]){
-                    apartNum++;
-                    bfs(i,j);
-                }
-            }
-        }
-
-        Arrays.sort(aparts);
-        System.out.println(apartNum);
-
-        for(int i=0; i<aparts.length; i++){
-            if(aparts[i] == 0){
-            }else{
-                System.out.println(aparts[i]);
-            }
-        }
-
-}
-
-    private static void bfs(int x, int y) {
-        //2차원 LinkedList를 가진 큐 선언
-        Queue<int[]> que = new LinkedList<>();
-        que.add(new int[]{x,y});
-        visited[x][y] = true;
-        aparts[apartNum]++;
-
-        while(!que.isEmpty()){
-
-            //x, y 값을 받아오기 위한 방법
-            int curX = que.peek()[0];
-            int curY = que.peek()[1];
-            que.poll();
-
-            for(int i=0; i<4; i++){
-                int nx = curX + dx[i];
-                int ny = curY + dy[i];
-
-                if(nx >= 0 && ny >= 0 && nx < n && ny < n){
-                    if(map[nx][ny] == 1 && !visited[nx][ny]){
-                        que.add(new int[]{nx,ny});
-                        visited[nx][ny] = true;
-                        aparts[apartNum]++;
-                    }
+            if (nx >= 0 && ny >= 0 && nx < N && ny < N) { // 지도 범위 안에 들었는지 확인
+                if (Apt.get(nx).charAt(ny) == '1' && !visited[nx][ny]) { // 아파트가 있고, 방문안한 곳
+                    DFS(nx, ny);
                 }
             }
         }
     }
 }
-*/
