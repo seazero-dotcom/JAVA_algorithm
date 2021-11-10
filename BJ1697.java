1b4f08406b2955b086ab1f397ae3fa7bf13818bd
seazero <withbb96@gmail.com>
2021-11-11 00:24:27 +0900

    BFS

--- BJ1697.java
+++ BJ1697.java
@@ -12,6 +12,7 @@
 //최단거리 문제와 유사하여 BFS로 풀이
 
 import java.util.*;
+import java.io.*;
 
 public class BJ1697 {
 
@@ -19,40 +20,46 @@ public class BJ1697 {
     private static int K; // 동생 위치
     private static int[] map = new int[100001];
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        // Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        N = sc.nextInt();
-        K = sc.nextInt();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
 
         bfs();
+
         System.out.println(map[K]);
+
+        br.close();
     }
 
-    static void bfs() {
+    public static void bfs() {
         Queue<Integer> que = new LinkedList<>();
 
-        que.offer(N);
+        que.offer(N); // 수빈이 위치 추가
 
         while (!que.isEmpty()) {
-            N = que.poll();
+            N = que.poll(); // 가장 먼저 보관한 값 꺼내고
 
-            if (N == K) {
+            if (N == K) { // 수빈이와 동생의 위치가 같을 때
                 break;
             }
 
-            if (N - 1 >= 0 && map[N - 1] == 0) {
-                que.offer(N - 1);
+            if (N - 1 >= 0 && map[N - 1] == 0) { // 수빈이가 걷고 방문 안한곳
+                que.offer(N - 1); // 수빈이 걷고 추가
                 map[N - 1] = map[N] + 1;
             }
 
-            if (N + 1 <= 100000 && map[N + 1] == 0) {
-                que.offer(N + 1);
+            if (N + 1 <= 100000 && map[N + 1] == 0) { // 수빈이가 걷고 방문 안한곳
+                que.offer(N + 1); // 수빈이 걷고 추가
                 map[N + 1] = map[N] + 1;
             }
 
-            if (N * 2 <= 100000 && map[N * 2] == 0) {
-                que.offer(N * 2);
+            if (N * 2 <= 100000 && map[N * 2] == 0) { // 수빈이가 순간이동하고 방문안한 곳
+                que.offer(N * 2); // 수빈이 순간이동하고 추가
                 map[N * 2] = map[N] + 1;
             }
         }
